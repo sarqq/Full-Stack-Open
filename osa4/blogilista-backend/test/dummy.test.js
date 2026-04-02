a0e918a77e5fd17a115853a7542fdd9ba362980a
sarqq <75566902+sarqq@users.noreply.github.com>
2026-04-02 14:59:14 +0300

    Add exercise 4.4

--- osa4/blogilista-backend/test/dummy.test.js
+++
@@ -1,9 +0,0 @@
-const { test, describe } = require('node:test')
-const assert = require('node:assert')
-const listHelper = require('../utils/list_helper')
-
-test('dummy returns 1', () => {
-    const blogs = []
-    
-    assert.strictEqual(listHelper.dummy(blogs), 1)
-})
--- osa4/blogilista-backend/utils/list_helper.js
+++ osa4/blogilista-backend/utils/list_helper.js
@@ -1,5 +1,16 @@
 const dummy = (blogs) => {
-    return 1
+   return 1
 }
 
-module.exports = {dummy}
+// 4.4: palauttaa listan blogien tykkäysten summan
+const totalLikes = (blogs) => {
+   // tyhjä blogilista -> 0
+   if (!blogs) {
+      return 0
+   }
+
+   const sum = blogs.reduce((acc, curr) => acc + curr.likes, 0)
+   return sum
+}
+
+module.exports = {dummy, totalLikes}
