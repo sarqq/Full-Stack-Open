7fdebbbc00d145b89c7ba9bc449f49b9aaad1417
sarqq <75566902+sarqq@users.noreply.github.com>
2026-05-05 04:34:26 +0300

    Add exercise 5.31

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,16 +1,11 @@
 import {useParams} from 'react-router-dom'
 import Togglable from './Togglable'
 
+import {Card, Button, Typography, CardContent, CardActions} from '@mui/material'
+
 const Blog = ({ blogs, handleLikes, handleRemove, user }) => {
    const id = useParams().id
    const blog = blogs.find(b => b.id === id)
-
-   const blogStyle = {
-      border: 'solid',
-      borderWidth: 1,
-      paddingTop: 5,
-      paddingLeft: 5
-   }
 
    // 5.8: blogin tykkäysten päivitys
    const handleLikeClick = (event) => {
@@ -34,26 +29,27 @@ const Blog = ({ blogs, handleLikes, handleRemove, user }) => {
    }
 
    return (
-      <div className="blog" style={blogStyle}>
-         <h3>{blog.title}</h3>
-         <Togglable buttonLabelOn="View" buttonLabelOff="Hide">
-            <div>
-               url: <a href={blog.url}>{blog.url}</a>
-            </div>
-            <div>
-               likes: {blog.likes}
-               {user && (
-                  <button onClick={handleLikeClick}>Like</button>
-               )}
-            </div>
-            <div>
-               author: {blog.author}
-            </div>
-               {user && (
-                  <button onClick={handleDelClick}>Remove</button>
-               )}
-         </Togglable>
-      </div>
+      <Card className="blog">
+         <CardContent>
+            <Typography gutterBottom variant="h5">{blog.title}</Typography>
+            <Typography variant="body2">
+               <ul>
+                  <li>url: <a href={blog.url}>{blog.url}</a></li>
+                  <li>likes: {blog.likes}</li>
+                  <li>author: {blog.author}</li>
+               </ul>
+            </Typography>
+         </CardContent>
+
+         <CardActions>   
+            {user && (
+               <Button size="small" onClick={handleLikeClick}>Like</Button>
+            )}
+            {user && (
+               <Button size="small" onClick={handleDelClick}>Remove</Button>
+            )}
+         </CardActions>
+      </Card>
    )
 }
 
